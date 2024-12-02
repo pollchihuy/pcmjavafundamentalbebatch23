cced4e307ef26cf40bcf7dae6b16d19fc42ad458
pollchihuy <poll.chihuy@gmail.com>
2024-12-02 19:28:33 +0700

    Push Class Contoh 2 Part 2

--- src/main/java/com/juaracoding/Contoh2.java
+++ src/main/java/com/juaracoding/Contoh2.java
@@ -13,5 +13,8 @@ public class Contoh2 {
 
     public static void main(String[] args) {
         System.out.println("Buat Push !!");
+        System.out.println("Buat Push !!");
+        System.out.println("Buat Push !!");
+        System.out.println("Buat Push !!");
     }
 }
