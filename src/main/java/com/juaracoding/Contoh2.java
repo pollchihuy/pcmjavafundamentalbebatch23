87433acdf5cb58377360b3e555a682a0cc094a03
pollchihuy <poll.chihuy@gmail.com>
2024-12-02 19:26:52 +0700

    Push Class Contoh 2

---
+++ src/main/java/com/juaracoding/Contoh2.java
@@ -0,0 +1,13 @@
+package com.juaracoding;
+
+/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author pollc a.k.a. Paul Christian
+Java Developer
+Created on Mon 19:25
+@Last Modified Mon 19:25
+Version 1.0
+*/
+public class Contoh2 {
+}
